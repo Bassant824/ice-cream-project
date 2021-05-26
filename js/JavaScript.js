cdb75bc7e3b49147721b7bf33ca4e4396fc0a5e5
Bassant824 <84861267+Bassant824@users.noreply.github.com>
2021-05-26 18:30:15 +0200

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,22 @@
+//using FontAwesome---------------->
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
+
+     type="text/javascript" ,src="js/JQuery3.3.1.js"> 
+
+    <script type="text/javascript">
+
+        /*Responsive Navigation*/
+
+        $(document).ready(function() {
+
+            $('.toggle').click(function() {
+
+                $('.toggle').toggleClass('active')
+
+                $('nav ul').toggleClass('active-menu')
+
+
+            })
+        });
+        
+    </script>
